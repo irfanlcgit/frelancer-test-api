27f741dc7f86f57cce2c2a1c9c2b97869ac17f91
irfan <mirfan@leadconcept.com>
2018-12-13 17:53:22 +0500

    add api for get user by email

--- users-get-by-email.js
+++ users-get-by-email.js
@@ -2,6 +2,7 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
     // 'KeyConditionExpression' defines the condition for the query
@@ -10,16 +11,30 @@ export async function main(event, context) {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
-    KeyConditionExpression: "userId = :userId",
+    KeyConditionExpression: "EmailAddres = :EmailAddress",
     ExpressionAttributeValues: {
-      ":userId": event.requestContext.identity.cognitoIdentityId
+      ":EmailAddress": data.EmailAddres
     }
   };
-  
+
   try {
     const result = await dynamoDbLib.call("query", params);
-    // Return the matching list of items in response body
-    return success(result.Items);
+    if (result.Items.length > 0) {
+      // Return the retrieved items
+      var items,i;
+      var item = 'Item not found.';
+      var status = false;
+      items=result.Items
+      for(i = 0; i < items.length; i++){
+        if('Password' in items[i]){
+            item = items[i];
+            status = true;
+        }
+      }
+        return success({ status: status, "item": item });
+      } else {
+        return success({ status: false, error: "Item not found." });
+      }
   } catch (e) {
     return failure({ status: false });
   }
